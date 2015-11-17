c43a973426a9e7871eba80dd20bedcdc3aaafd8e
brennanblue <brennanblue@gmail.com>
2015-11-16 18:08:39 -1000

    Added read line function that prints a line

--- readFile.py
+++ readFile.py
@@ -10,6 +10,13 @@ def file_len(fname):
             pass
     return i + 1
 
+def get_line(num):
+	with open(from_file) as f:
+	    for i, line in enumerate(f, 1):
+	        if i == num:
+	            break
+	return line
+
 script, from_file = argv
 
 print "Reading from %s" % (from_file)
@@ -25,10 +32,20 @@ for char in indata:
 #	print "line is : %s" % (line)
 	char_count +=1
 
+# num = 2
+# with open(from_file) as f:
+#     for i, line in enumerate(f, 1):
+#         if i == num:
+#             break
+# print line
+
+
 # char_count = sum(1 for line in in_file)
 
 # char_count = sum(1 for line in open(in_file))
 
 print "There are %d characters in the file." % (char_count)
 print "The length of the file is %d lines" % (file_len(from_file))
+
+print "Line 2 says %s" % (get_line(2))
 in_file.close()
