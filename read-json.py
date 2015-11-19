fcb24221170083ef8fe2e5a68802f6ce0bf006a3
brennanblue <brennanblue@gmail.com>
2015-11-18 17:36:33 -1000

    now printing key names. How to access values?

--- read-json.py
+++ read-json.py
@@ -13,15 +13,16 @@ def main():
     # Read file
     with open(args.filename, 'r') as json_file:
         contents = json_file.read()
-        catalog = json.loads(contents)
+        choices = json.loads(contents)
 
     # Make sure that worked
-    if not catalog:
+    if not choices:
         sys.stderr.write("Error reading json file\n")
         sys.exit()
 
-#cache subject of imported file
-# subject = catalog['abbr']
+    for item in choices:
+        print item
+
 print "Imported file successfully. That is all"
 # Create a dictionary that maps subjects, courses
     # subject courses = {}
