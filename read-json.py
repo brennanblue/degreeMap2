17ec9ef330e8be8f7399a29413483c8c21eb299e
brennanblue <brennanblue@gmail.com>
2015-11-21 22:07:51 -1000

    added new test import; switched % to .format, cleanup

--- read-json.py
+++ read-json.py
@@ -3,6 +3,7 @@
 import sys
 import json
 import argparse
+# from collections import namedtuple
 
 def main():
     # Parse command line arguments
@@ -34,7 +35,7 @@ def main():
             courses[slot] = choices[i][-9:].strip()
             parts = courses[slot].split("_")
             subjects[parts[1]] = parts[0]
-            string = " %s - %s; " % (parts[0], parts[1])
+            string = "{} {}".format(parts[0], parts[1])
             l.append(string)
             # subjects[parts[0]].append(subjects[parts[1]])
         elif (i[:5] == 'slot-'):
@@ -46,46 +47,12 @@ def main():
                 string = " %s: - %s; " % (parts[0], parts[1])
                 l.append(string)
 
-    s = ''.join(l)
+    print "There are {} slots with courses specified".format(slot)
+
+    s = "\n".join(l)
     print s
 
-    print "There are %d slots with courses specified" % slot
-    print "A total of %d courses were chosen" % len(courses)
-
-    # print subjects.items
-    for k, v in subjects.iteritems():
-        print k, v
-
-    print subjects.keys() # will give you all keys in list
-    print subjects.values() # will give you values in list
-    print subjects.items()  # will give you pair tuple of key and value
-    # for i in courses:
-    #     print courses[i]
-
-    # for i in subjects:
-    #     print subjects[i]
-
-    # Read through entries, determine if they are courses
-    # for item in catalog['courses']:
-    #     courses
-    #     if item['type'] == 'message' and '?' in item['text']:
-    #         # Update dict
-    #         user = item['user']
-    #         if user in questions:
-    #             questions[user] += 1
-    #         else:
-    #             questions[user] = 1
-
-    # # Turn that dict into a list of (user, question_count) tuples
-    # q_list = []
-    # for user, count in questions.items():
-    #     q_list.append( (user, count) )
-
-    # # Sort the list
-    # q_list = sorted(q_list, key = lambda x: x[1], reverse=True)
-
-    # # Print results
-    # print(q_list)
+    print "A total of {} courses were chosen".format(len(courses))
 
 def isset(variable):
     return variable in locals() or variable in globals()
